6c45de55752b3f8023b91ac662a20a1fd1ba5d3f
Amir Shariff <amirshariff@msn.com>
2020-08-21 23:02:30 +0100

    persist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import TodosForm from './Components/TodoForm'
+import TodoList from './Components/TodoList';
+
+const LOCAL_STORAGE_KEY = "react-todo-list=todos"
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storageTodos) {
+        setTodos(storageTodos)
+    }
+  },[])
+
+  useEffect(() => {
+   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+ },[todos])
+
+  const addTodo = (todo) => {
+    setTodos([todo,...todos])
+  }
+
+   
   return (
     <div className="App">
-     
+      <p>React Todo</p>
+      <TodosForm addTodo={addTodo} />
+      <TodoList todos={todos}/>
     </div>
   );
 }
